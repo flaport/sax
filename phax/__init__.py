238fbe2d04b8f3305a88bdd4629fa759d3e74c04
flaport <floris.laporte@gmail.com>
2020-09-25 14:50:31 +0200

    Initial repository structure

---
+++ phax/__init__.py
@@ -0,0 +1,3 @@
+""" phax """
+__author__ = "Floris Laporte"
+__version__ = "0.0.0"
---
+++ setup.py
@@ -0,0 +1,25 @@
+import phax
+import setuptools
+
+setuptools.setup(
+    name=phax.__name__,
+    version=phax.__version__,
+    description=phax.__doc__,
+    long_description=phax.__doc__,
+    author=phax.__author__,
+    author_email="floris.laporte@gmail.com",
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Scientific/Engineering",
+        "Topic :: Scientific/Engineering :: Physics",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+        "License :: OSI Approved :: Apache Software License"
+    ],
+)
