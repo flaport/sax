3438f2d38e72928af4474b17dfc2cf946fbe9b99
flaport <floris.laporte@gmail.com>
2021-04-01 14:37:21 +0200

    update jaxinstall.sh script

--- jaxinstall.sh
+++ jaxinstall.sh
@@ -3,23 +3,25 @@
 [ -z "$PLATFORM_VERSION" ] && PLATFORM_VERSION="manylinux2010_x86_64"
 [ -z "$PYTHON_VERSION" ] && PYTHON_VERSION="cp$(python --version | sed 's/^.*[ ]\([0-9]\)\.\([0-9]\).*/\1\2/g')"
 [ -z "$CUDA_VERSION" ] && CUDA_VERSION="nocuda"
-[ -z "$JAXLIB_VERSION" ] && JAXLIB_VERSION="$(grep "jaxlib" requirements.txt | sed 's/==/-/g')"
-JAXLIB_URL=$JAXLIB_VERSION
+[ -z "$JAX_VERSION" ] && JAX_VERSION="$(grep "\bjax\b" requirements.txt)"
+[ -z "$JAXLIB_VERSION" ] && JAXLIB_VERSION="$(grep "jaxlib" requirements.txt)"
+JAXLIB_URL=$(echo $JAXLIB_VERSION | sed 's/==/-/g')
 if which nvcc > /dev/null 2>&1; then
     [ "$CUDA_VERSION" == "nocuda" ] && CUDA_VERSION="cuda$(nvcc --version | grep release | sed 's/^.*release[ ]\([0-9]*\)\.\([0-9]*\).*$/\1\2/g')"
-    JAXLIB_URL="https://storage.googleapis.com/jax-releases/$CUDA_VERSION/$JAXLIB_VERSION+$CUDA_VERSION-$PYTHON_VERSION-none-$PLATFORM_VERSION.whl"
+    JAXLIB_URL="https://storage.googleapis.com/jax-releases/$CUDA_VERSION/$JAXLIB_URL+$CUDA_VERSION-$PYTHON_VERSION-none-$PLATFORM_VERSION.whl"
 fi
 PYTHON=$(which python)
 
 echo "python version: $PYTHON_VERSION @ $PYTHON"
 echo "platform version: $PLATFORM_VERSION"
 echo "cuda version: $CUDA_VERSION"
+echo "jax version: $JAX_VERSION"
 echo "jaxlib version: $JAXLIB_VERSION"
 echo
-echo "pip install --upgrade jax"
+echo "pip install --upgrade $JAX_VERSION"
 echo "pip install --upgrade $JAXLIB_URL"
 echo
 echo
 
-$PYTHON -m pip install --upgrade jax
+$PYTHON -m pip install --upgrade $JAX_VERSION
 $PYTHON -m pip install --upgrade $JAXLIB_URL
