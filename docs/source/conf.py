9f86ea53af097540f1947e21fb3e69911ac74862
flaport <floris.laporte@gmail.com>
2023-09-18 12:47:04 -0700

    Bump version: 0.10.1 → 0.10.2

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sax  # noqa: E402
 project = "sax"
 copyright = "2023, Apache2"
 author = "Floris Laporte"
-release = "0.10.1"
+release = "0.10.2"
 extensions = [
     "myst_nb",
     "matplotlib.sphinxext.plot_directive",
--- sax/__init__.py
+++ sax/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 __author__ = "Floris Laporte"
-__version__ = "0.10.1"
+__version__ = "0.10.2"
 
 
 from functools import partial as partial
