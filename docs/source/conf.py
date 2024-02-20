d8bd85b3bb1d7383de228ec0c4e14fd4ef5d3609
flaport <floris.laporte@gmail.com>
2024-02-19 18:27:31 -0800

    Bump version: 0.11.3 → 0.11.4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -19,7 +19,7 @@ import sax  # noqa: E402
 project = "sax"
 copyright = "2023, Apache2"
 author = "Floris Laporte"
-release = "0.11.3"
+release = "0.11.4"
 extensions = [
     "myst_nb",
     "matplotlib.sphinxext.plot_directive",
