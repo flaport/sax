7c586e554b778d97fdb4f04319a10a832420a636
flaport <floris.laporte@gmail.com>
2025-06-18 12:25:40 +0200

    add __all__ in backends/__init__.py

--- src/sax/backends/__init__.py
+++ src/sax/backends/__init__.py
@@ -8,32 +8,38 @@ from typing import Any, cast
 from ..saxtypes import Backend, Model, SType
 from ..saxtypes.netlist import Instance
 from .additive import (
-    analyze_circuit_additive as analyze_circuit_additive,
-)
-from .additive import (
-    analyze_instances_additive as analyze_instances_additive,
-)
-from .additive import (
-    evaluate_circuit_additive as evaluate_circuit_additive,
+    analyze_circuit_additive,
+    analyze_instances_additive,
+    evaluate_circuit_additive,
 )
 from .filipsson_gunnar import (
-    analyze_circuit_fg as analyze_circuit_fg,
-)
-from .filipsson_gunnar import (
-    analyze_instances_fg as analyze_instances_fg,
-)
-from .filipsson_gunnar import (
-    evaluate_circuit_fg as evaluate_circuit_fg,
+    analyze_circuit_fg,
+    analyze_instances_fg,
+    evaluate_circuit_fg,
 )
 from .forward_only import (
-    analyze_circuit_forward as analyze_circuit_forward,
+    analyze_circuit_forward,
+    analyze_instances_forward,
+    evaluate_circuit_forward,
 )
-from .forward_only import (
-    analyze_instances_forward as analyze_instances_forward,
-)
-from .forward_only import (
-    evaluate_circuit_forward as evaluate_circuit_forward,
-)
+
+__all__ = [
+    "analyze_circuit",
+    "analyze_circuit_additive",
+    "analyze_circuit_fg",
+    "analyze_circuit_forward",
+    "analyze_instances",
+    "analyze_instances_additive",
+    "analyze_instances_fg",
+    "analyze_instances_forward",
+    "backend_map",
+    "circuit_backends",
+    "evaluate_circuit",
+    "evaluate_circuit_additive",
+    "evaluate_circuit_fg",
+    "evaluate_circuit_forward",
+    "validate_circuit_backend",
+]
 
 circuit_backends = {
     "fg": (
