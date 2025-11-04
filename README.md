f6d11a0bad89a52a11ec4e421e1296145b6c5a3e
flaport <floris.laporte@gmail.com>
2025-11-04 13:01:07 +0100

    Bump to 0.15.15

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # SAX
 
-> 0.15.14
+> 0.15.15
 
 ![SAX LOGO](docs/assets/logo.svg)
 
--- src/sax/__init__.py
+++ src/sax/__init__.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 __author__ = "Floris Laporte"
-__version__ = "0.15.14"
+__version__ = "0.15.15"
 
 # isort: off
 from .saxtypes import (
